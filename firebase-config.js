0825e2cc162e389bc5c746a270555d9f39380141
colorLabth <hnungh304@gmail.com>
2025-06-15 14:41:09 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDPPVx8ghs5SD0QH6h0Lxw2I0YOv2A9ccM",
   authDomain: "allunastore.firebaseapp.com",
   projectId: "allunastore",
-  storageBucket: "allunastore.firebasestorge.app",
+  storageBucket: "allunastore.firebasestorage.app",
   messagingSenderId: "839489402494",
   appId: "1:839489402494:web:7fc04e228ad456ba97ca2f",
   measurementId: "G-C3ZTR2BVT4"
