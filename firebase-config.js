46be6462e9c73cfec456d05f4811f49e6f139cb0
colorLabth <hnungh304@gmail.com>
2025-06-15 13:45:00 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyDPPVx8ghs5SD0QH6h0Lxw2I0YOv2A9ccM",
+  authDomain: "allunastore.firebaseapp.com",
+  projectId: "allunastore",
+  storageBucket: "allunastore.firebasestorge.app",
+  messagingSenderId: "839489402494",
+  appId: "1:839489402494:web:7fc04e228ad456ba97ca2f",
+  measurementId: "G-C3ZTR2BVT4"
+};
+
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+</script>
